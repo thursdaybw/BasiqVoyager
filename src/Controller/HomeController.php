c01bceb67968dd4df8befa9fb324d964be9ce273
Bevan Wishart <bevan.wishart@gmail.com>
2023-08-10 02:33:38 +1000

    More refactor

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -19,59 +19,24 @@ class HomeController extends AbstractController {
     private $basiqUserService;
     private $accountProcessingService;
 
-    public function __construct(BasiqUserService $basiqUserService, AccountProcessingService $accountProcessingService)
-    {
+    public function __construct(BasiqUserService $basiqUserService, AccountProcessingService $accountProcessingService) {
         $this->basiqUserService = $basiqUserService;
         $this->accountProcessingService = $accountProcessingService;
     }
 
     #[Route('/', name: 'home')]
-    public function index(Request $request): Response
-    {
+    public function index(Request $request): Response {
         $form = $this->createConnectBankForm();
-
-       $form->handleRequest($request);
+        $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-
             $consents = $this->basiqUserService->getUserConsents(BASIC_TEST_USER_ID);
             $consent_errors_rendered = $this->handleConsentErrors($consents);
 
-            if (isset($consents['data']) && !empty($consents['data'])) {
-                if ($user = $this->basiqUserService->fetchUserDetails(BASIC_TEST_USER_ID)) {
-                    $user_model = new User($user);
-                    $account_links = $user_model->getAccountLinks();
-                    $accounts = [];
-                    foreach ($account_links as $account_link) {
-                        $accounts[] = $this->basiqUserService->fetchUsersAccount($account_link);
-                    }
-
-                    $accounts = $this->accountProcessingService->setDefaultValuesForMissingKeysOfAccounts($accounts);
-
-                    $accounts_rendered = $this->render(
-                        'home/accounts.html.twig',
-                        [
-                            'accounts' => $accounts,
-                        ]
-                    );
-
-                    $main_content_rendered = $this->render(
-                        'home/main.html.twig',
-                        [
-                            'user' => $user,
-                            'accounts' => $accounts_rendered,
-                            'errors' => $consent_errors_rendered ?? null,
-                        ]
-                    );
-                } else {
-                    $main_content_rendered = "Error fetching user details.";
-                }
-            } else {
-                $main_content_rendered = "No consents found for the user.";
-            }
+            $main_content_rendered = $this->handleMainContent($consents, $consent_errors_rendered);
 
             return $this->render('home/index.html.twig', [
-                'form' => NULL, 
+                'form' => NULL,
                 'showForm' => FALSE,
                 'main' => $main_content_rendered,
             ]);
@@ -82,53 +47,63 @@ class HomeController extends AbstractController {
             'showForm' => TRUE,
             'main' => 'Nothing happened.',
         ]);
-
     }
 
-    private function processAccounts(array $accounts): array {
-      $account_required_keys = [
-        'balance', 'availableFunds', 'lastUpdated',
-        'creditLimit', 'type', 'product',
-        'accountHolder', 'status',
-      ];
+    private function handleMainContent($consents, $consent_errors_rendered) {
+        if (isset($consents['data']) && !empty($consents['data'])) {
+            if ($user = $this->basiqUserService->fetchUserDetails(BASIC_TEST_USER_ID)) {
+                $user_model = new User($user);
+                $accounts = $this->processAccounts($user_model->getAccountLinks());
+                return $this->renderMainContent($user, $accounts, $consent_errors_rendered);
+            } else {
+                return "Error fetching user details.";
+            }
+        } else {
+            return "No consents found for the user.";
+        }
+    }
 
-      foreach ($accounts as &$account) {
-        foreach ($account_required_keys as $required_key) {
-            if (!isset($account[$required_key])) {
-                $account[$required_key] = NULL;
+    private function renderMainContent($user, $accounts, $consent_errors_rendered) {
+        $accounts_rendered = $this->render('home/accounts.html.twig', ['accounts' => $accounts]);
+        return $this->render('home/main.html.twig', [
+            'user' => $user,
+            'accounts' => $accounts_rendered,
+            'errors' => $consent_errors_rendered ?? null,
+        ]);
+    }
+
+    private function processAccounts(array $account_links): array {
+        $accounts = [];
+        foreach ($account_links as $account_link) {
+            $accounts[] = $this->basiqUserService->fetchUsersAccount($account_link);
+        }
+        return $this->accountProcessingService->setDefaultValuesForMissingKeysOfAccounts($accounts);
+    }
+
+    private function createConnectBankForm(): FormInterface {
+        return $this->createFormBuilder()
+            ->add('connectBank', SubmitType::class, ['label' => 'Connect Bank'])
+            ->getForm();
+    }
+
+    private function handleConsentErrors(array $consents): ?Response {
+        if (isset($consents['data']) && !empty($consents['data'])) {
+            $errors = array_filter($consents['data'], function ($item) {
+                return isset($item['type']) && $item['type'] === 'error';
+            });
+    
+            if (!empty($errors)) {
+                error_log("Error found in the 'data' key of the response.");
+                return $this->render(
+                    'consent_errors.html.twig',
+                    [
+                        'correleation_id' => $consents['correlationId'],
+                        'errors' => print_r($errors, 1),
+                    ]
+                );
             }
         }
-      }
-
-      return $accounts;
-   }
-
-   private function createConnectBankForm(): FormInterface {
-    return $this->createFormBuilder()
-        ->add('connectBank', SubmitType::class, ['label' => 'Connect Bank'])
-        ->getForm();
-   }
-
-   private function handleConsentErrors(array $consents): ?Response {
-    if (isset($consents['data']) && !empty($consents['data'])) {
-        $errors = array_filter($consents['data'], function ($item) {
-            return isset($item['type']) && $item['type'] === 'error';
-        });
-
-        if (!empty($errors)) {
-            error_log("Error found in the 'data' key of the response.");
-            return $this->render(
-                'consent_errors.html.twig',
-                [
-                    'correleation_id' => $consents['correlationId'],
-                    'errors' => print_r($errors, 1),
-                ]
-            );
-        }
+        return null;
     }
-
-    return null;
-   }
-
 
 }
